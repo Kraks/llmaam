aaecc2283e996036cc006139be489dbb342f9a7f
Guannan Wei <guannan.wei@tufts.edu>
2025-06-06 23:26:55 +0200

    comment llm test

--- src/test/scala/TestAAM.scala
+++ src/test/scala/TestAAM.scala
@@ -21,7 +21,7 @@ class Playground extends FunSuite {
   //test("stack2 - 2cfa; p4f"):
   //  testAnalyzer(new Analyzer with KCFA(2) with P4FContAlloc, benchmarks.stack2, "stack2_2cfa_p4f")
 
-  testAnalyzer(new Analyzer with LLMAllocator, benchmarks.omega, "llm_exp")
+  //testAnalyzer(new Analyzer with LLMAllocator, benchmarks.omega, "llm_exp")
 }
 
 class TestAAM extends FunSuite {
